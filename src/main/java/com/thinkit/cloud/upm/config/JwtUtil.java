7ca92cfcf9352d44a077ede6e220c7400056c55b
liujie <liujie@thinkit.cn>
2019-07-15 18:10:20 +0800

    添加RSA加密

--- src/main/java/com/thinkit/cloud/upm/config/JwtUtil.java
+++ src/main/java/com/thinkit/cloud/upm/config/JwtUtil.java
@@ -6,11 +6,18 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import com.thinkit.cloud.upm.bean.JWTInfo;
+import com.thinkit.cloud.upm.util.RsaKeyHelper;
+import com.thinkit.cloud.upm.util.StringHelper;
+import com.zhongkexinli.micro.serv.common.constant.CommonConstants;
 import com.zhongkexinli.micro.serv.common.util.StringUtil;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,6 +30,8 @@ public class JwtUtil {
     @Value("${jtwTokenPrefix}")
     public   String tokenPrefix;
     public static final String HEADER_STRING = "TOKEN";
+    
+    private static RsaKeyHelper rsaKeyHelper = new RsaKeyHelper();
     
     public  String generateToken(String username,String userId, Long jtwTokenTimeOut) {
         HashMap<String, Object> map = new HashMap<>();
@@ -69,5 +78,74 @@ public class JwtUtil {
         public TokenValidationException(String msg) {
             super(msg);
         }
+    }
+    
+    /**
+     * 密钥加密token
+     *
+     * @param jwtInfo
+     * @param priKey
+     * @param expire
+     * @return
+     * @throws Exception
+     */
+    public static String generateToken(IJWTInfo jwtInfo, byte priKey[], int expire) throws Exception {
+        String compactJws = Jwts.builder()
+                .setSubject(jwtInfo.getUniqueName())
+                .claim(CommonConstants.JWT_KEY_USER_ID, jwtInfo.getId())
+                .claim(CommonConstants.JWT_KEY_NAME, jwtInfo.getName())
+                .setExpiration(DateTime.now().plusSeconds(expire).toDate())
+                .signWith(SignatureAlgorithm.RS256, rsaKeyHelper.getPrivateKey(priKey))
+                .compact();
+        return compactJws;
+    }
+
+    /**
+     * 公钥解析token
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public static Jws<Claims> parserToken(String token, String pubKeyPath) throws Exception {
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(rsaKeyHelper.getPublicKey(pubKeyPath)).parseClaimsJws(token);
+        return claimsJws;
+    }
+    /**
+     * 公钥解析token
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public static Jws<Claims> parserToken(String token, byte[] pubKey) throws Exception {
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(rsaKeyHelper.getPublicKey(pubKey)).parseClaimsJws(token);
+        return claimsJws;
+    }
+    /**
+     * 获取token中的用户信息
+     *
+     * @param token
+     * @param pubKeyPath
+     * @return
+     * @throws Exception
+     */
+    public static IJWTInfo getInfoFromToken(String token, String pubKeyPath) throws Exception {
+        Jws<Claims> claimsJws = parserToken(token, pubKeyPath);
+        Claims body = claimsJws.getBody();
+        return new JWTInfo(body.getSubject(), StringHelper.getObjectValue(body.get(CommonConstants.JWT_KEY_USER_ID)), StringHelper.getObjectValue(body.get(CommonConstants.JWT_KEY_NAME)));
+    }
+    /**
+     * 获取token中的用户信息
+     *
+     * @param token
+     * @param pubKey
+     * @return
+     * @throws Exception
+     */
+    public static IJWTInfo getInfoFromToken(String token, byte[] pubKey) throws Exception {
+        Jws<Claims> claimsJws = parserToken(token, pubKey);
+        Claims body = claimsJws.getBody();
+        return new JWTInfo(body.getSubject(), StringHelper.getObjectValue(body.get(CommonConstants.JWT_KEY_USER_ID)), StringHelper.getObjectValue(body.get(CommonConstants.JWT_KEY_NAME)));
     }
 }
