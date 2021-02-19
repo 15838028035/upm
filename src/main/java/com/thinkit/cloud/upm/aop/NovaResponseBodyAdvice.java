459e5de38297b4b709e1bb972e73615acdedfb0d
15838028035@163.com <15838028035@163.com>
2021-02-19 16:24:00 +0800

    resultApiResult2添加traceId返回参数

---
+++ src/main/java/com/thinkit/cloud/upm/aop/NovaResponseBodyAdvice.java
@@ -0,0 +1,43 @@
+package com.thinkit.cloud.upm.aop;
+
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import com.zhongkexinli.micro.serv.common.bean.RestApiResult2;
+
+@ControllerAdvice("com.thinkit.cloud.upm.controller")
+public class NovaResponseBodyAdvice implements ResponseBodyAdvice {
+	private final static String LOG_TRACE_ID = "logTraceId";
+	
+ @Override
+ public boolean supports(MethodParameter returnType, Class converterType) {
+	 String uuid = UUID.randomUUID().toString();
+     MDC.put(LOG_TRACE_ID, uuid);
+     
+     return true;
+ }
+
+ @Override
+ public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+         Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+  
+  if( body instanceof RestApiResult2) {
+	  String traceId = MDC.get(LOG_TRACE_ID);
+	  RestApiResult2 restApiResult2 = ((RestApiResult2)body);
+	  restApiResult2.setTraceId(traceId);
+      return restApiResult2;
+  }
+  
+  MDC.remove(LOG_TRACE_ID);
+  
+  return body;
+ }
+
+}
